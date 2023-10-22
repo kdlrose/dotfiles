935eac57ac73c0c155aeec5c03b56cb285ddcf5a
sailor mars <sailorfe@proton.me>
2023-10-21 21:46:36 -0400

    better nehellenia bg

--- wezterm.lua
+++ wezterm.lua
@@ -10,14 +10,17 @@ config.enable_tab_bar = false
 
 config.font = wezterm.font 'Inconsolata'
 
--- 1440x900
-config.font_size = 16.0
+-- 1680x1050
+config.font_size = 13.0
 
---[[1366x768, 1680x1050
+--[[1440x900
+config.font_size = 16.0
+]]
+--[[1366x768
 config.font_size = 11.25
 ]]
 
-config.color_scheme = 'Serenity'
+config.color_scheme = 'Nehellenia'
 
 config.color_schemes = {
     ['Serenity'] = {
@@ -31,13 +34,13 @@ config.color_schemes = {
         scrollbar_thumb = '#819EC0',
         split           = '#819EC0',
         --                 black      red        green      yellow     blue       magenta    cyan       white
-        ansi            = {'#261d2d', '#ff0083', '#00a294', '#ec8300', '#0c1eb8', '#7800b8', '#ff69d3', '#d6c5d8'},
+        ansi            = {'#261d2d', '#ff0083', '#00a294', '#ec8300', '#0c1eb8', '#7800b8', '#ff69d3', '#e3d1e5'},
         brights         = {'#4d3a5a', '#ff0083', '#00a294', '#ec8300', '#0c1eb8', '#7800b8', '#ff69d3', '#e3d1e5'},
     },
     ['Nehellenia'] = {
         foreground      = '#fddbff',
-        background      = '#3c2e47',
-        cursor_bg       = '#76ffff',
+        background      = '#1e1f31',
+        cursor_bg       = '#B175AC',
         cursor_fg       = '#fddbff',
         cursor_border   = '#fddbff',
         selection_fg    = '#3c2e47',
@@ -45,8 +48,8 @@ config.color_schemes = {
         scrollbar_thumb = '#af748d',
         split           = '#af74bd',
         --                 black      red        green      yellow     blue       magenta    cyan       white
-        ansi            = {'#261d2d', '#F779B4', '#37816f', '#ffbc4d', '#67A8E4', '#DB92F4', '#FF84FC', '#ffe3f9'},
-        brights         = {'#261d2d', '#F779b4', '#37816f', '#ffbc4d', '#67A8E4', '#DB92F4', '#FF84FC', '#ffe3f9'},
+        ansi            = {'#261d2d', '#c72079', '#37816f', '#ffbc4d', '#819EC0', '#BF56CA', '#FF84FC', '#ffe3f8'},
+        brights         = {'#261d2d', '#c72079', '#37816f', '#ffbc4d', '#819EC0', '#BF56CA', '#FF84FC', '#ffe3f8'},
    },
 }
 
