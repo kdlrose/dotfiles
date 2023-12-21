4f662e640a70ed00b3184d9d96b2492c1baf1ea3
sailorfe <sailorfe@proton.me>
2023-12-21 15:49:55 -0500

    wezterm, bincho

--- wezterm.lua
+++ wezterm.lua
@@ -15,16 +15,16 @@ local function get_theme()
     local _time = os.date("*t")
     if _time.hour >= 1 and _time.hour < 9 then
         return "Moon Queen Nehellenia"
-    elseif _time.hour >= 9 and _time.hour < 18 then
+    elseif _time.hour >= 9 and _time.hour < 17 then
         return "Moon Queen Serenity"
-    elseif _time.hour >= 18 and _time.hour < 24 or _time.hour >= 0 and _time.hour < 1 then
+    elseif _time.hour >= 17 and _time.hour < 24 or _time.hour >= 0 and _time.hour < 1 then
         return "Moon Queen Nehellenia"
     end
 end
 
 return {
-    font = wezterm.font 'Inconsolata',
-    font_size = 13.0,
+    font = wezterm.font 'Binchotan_Sharp',
+    font_size = 15.0,
     color_scheme = get_theme(),
     enable_tab_bar = true,
     use_fancy_tab_bar = false
