52ccda44d907e868a5652f9a6f3ae142e312f232
sailorfe macos <sailorfe@proton.me>
2024-02-16 14:37:57 -0500

    wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -13,24 +13,15 @@ end)
 
 local function get_theme()
     local _time = os.date("*t")
-    if _time.hour >= 9 and _time.hour < 16 then
+    if _time.hour >= 9 and _time.hour < 18 then
         return "Moon Queen Serenity"
     end
     return "Moon Queen Nehellenia"
 end
 
-wezterm.on("update-right-status", function(window,pane)
-    local overrides = window:get_config_overrides() or {}
-    local color_scheme = get_theme()
-    if overrides.color_scheme ~= color_scheme then
-        overrides.color_scheme = color_scheme
-        window:set_config_overrides(overrides)
-    end
-end)
-
 return {
     font = wezterm.font 'Binchotan_Sharp',
-    font_size = 16.5,
+    font_size = 17,
     color_scheme = get_theme(),
     enable_tab_bar = true,
     use_fancy_tab_bar = false
